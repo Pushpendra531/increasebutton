f4274924017e3c0586a05f580c40532f0d5df0c8
Pushpendra531 <arvind8924900581@gmail.com>
2024-12-12 10:17:05 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+// import logo from './logo.svg';
+import './App.css';
+import File from './component/File';
+function App() {
+  return (
+    <div className="App">
+      <File/>
+    </div>
+  );
+}
+
+export default App;
